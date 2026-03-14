ec3ae6421aa45e25bdd6bc14f5fa7c370572770d
Your Name <sonetrue1@gmail.com>
2026-03-14 13:38:13 +0300

    Сделан небольшой html.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
 
-# Create your views here.
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=int(request.POST.get("quantity", 1)))
+    return redirect("cart_detail")
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, "cart/detail.html", {"cart": cart})
